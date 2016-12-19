9b7b6a7262459424618846538f3901dd50a64a16
liu-frank <lk5861412@sina.com>
2016-12-19 15:23:51 +0800

    vuex+vue-router demo

---
+++ src/pages/shop/index.vue
@@ -0,0 +1,31 @@
+<style lang="less" scoped>
+    .login-msg {
+        padding: 50px;
+        text-align: center;
+    }
+    .msg {
+        padding: 50px;
+        text-align: center;
+        font-size: 20px;
+        color: red;
+    }
+</style>
+<template>
+    <div>
+        <v-header title="首页">
+            <router-link slot="right" v-if="user.id" to="/home">{{user.name}}</router-link>
+        </v-header>
+        <div class="login-msg" v-if="!user.id">
+            <router-link to="/login">你还未登录，请先登录</router-link>
+        </div>
+        <div class="msg" v-if="user.id">
+           商户
+        </div>
+    </div>
+</template>
+<script>
+    import { mapState } from 'vuex'
+    export default {
+        computed: mapState({ user: state => state.user }),
+    }
+</script>
